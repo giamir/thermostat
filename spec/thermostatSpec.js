abe4f31eb09a35cac5d958a055301fdd57987825
vegim qehaja <med.batt.uk@gmail.com>
2016-01-06 11:54:28 +0000

    completed business logic

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use_strict';
+
 describe('Thermostat', function() {
 
   var thermostat;
@@ -8,8 +10,33 @@ describe('Thermostat', function() {
 
   describe('#initialize', function() {
     it('starts with a default temperature', function() {
-      expect(thermostat.temperature).toEqual(DEFAULT_TEMPERATURE);
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
     });
+
+    it('power saving mode is on by default', function(){
+      expect(thermostat.isPowerSavingModeOn).toBe(true);
+    });
+  });
+
+  describe('#getCurrentTemperature', function(){
+    it('returns current temperature', function(){
+    expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
+  });
+  });
+
+  describe('#switchPowerMode', function(){
+    it('Changes state of power saving mode true/false', function() {
+      thermostat.isPowerSavingModeOn = true;
+      thermostat.switchPowerMode();
+      expect(thermostat.isPowerSavingModeOn).toBe(false);
+    });
+    it('change temperature to max saving mode temp when switched on and previous temperature exceeded saving mode temp', function(){
+      thermostat.isPowerSavingModeOn = false;
+      thermostat.temperature = 30;
+      thermostat.switchPowerMode();
+      expect(thermostat.temperature).toEqual(thermostat.MAX_SAVING_MODE_TEMP);
+    });
+
   });
 
   describe('#increaseTemperature', function() {
@@ -18,6 +45,23 @@ describe('Thermostat', function() {
       thermostat.increaseTemperature();
       expect(thermostat.temperature).toEqual(temperature + 1);
     });
+
+   describe('when power saving mode is on', function() {
+     it('throw an error if maximum temperature is exceeded',function(){
+      thermostat.temperature = thermostat.MAX_SAVING_MODE_TEMP;
+      var msg = 'Unable to increase temperature because power saving mode is on';
+      expect( function(){thermostat.increaseTemperature();}).toThrowError(msg);
+     });
+   });
+
+   describe('when power saving mode is off', function() {
+     it('throw an error if maximum temperature is exceeded',function(){
+      thermostat.isPowerSavingModeOn = false;
+      thermostat.temperature = thermostat.MAX_TEMPERATURE;
+      var msg = 'Unable to increase temperature because maxmimum temperature reached';
+      expect( function(){thermostat.increaseTemperature();}).toThrowError(msg);
+     });
+   });
   });
 
   describe('#decreaseTemperature', function() {
@@ -27,10 +71,38 @@ describe('Thermostat', function() {
       expect(thermostat.temperature).toEqual(temperature - 1);
     });
     it('throw an error temperature is set to min', function() {
-      thermostat.temperature = MIN_TEMPERATURE;
+      thermostat.temperature = thermostat.MIN_TEMPERATURE;
       var msg = 'Unable to decrease temperature cause is already set to min';
       expect( function(){ thermostat.decreaseTemperature(); } ).toThrowError(msg);
     });
+  });
+
+  describe('#resetTemperature', function() {
+    it('resets temperature to default temperature value',function(){
+      thermostat.increaseTemperature();
+      thermostat.resetTemperature();
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
+    });
+  });
+
+  describe('#energyUsageStatus', function(){
+    beforeEach(function() {
+      thermostat.isPowerSavingModeOn = false;
+    });
+
+    it('returns low if temperature below energy usage low level', function(){
+      thermostat.temperature = 17;
+      expect(thermostat.energyUsageStatus()).toEqual('low');
+    });
+    it('returns medium if temperature below energy usage medium level', function(){
+      thermostat.temperature = 18;
+      expect(thermostat.energyUsageStatus()).toEqual('medium');
+    });
+    it('returns high if temperature above energy usage medium level', function(){
+      thermostat.temperature = 25;
+      expect(thermostat.energyUsageStatus()).toEqual('high');
+    });
+
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,19 +1,64 @@
-var DEFAULT_TEMPERATURE = 20;
-var MIN_TEMPERATURE = 10;
+'use_strict';
 
 function Thermostat() {
-  this.temperature = DEFAULT_TEMPERATURE;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.MIN_TEMPERATURE = 10;
+  this.MAX_SAVING_MODE_TEMP = 25;
+  this.MAX_TEMPERATURE = 32;
+  this.ERROR_MSG = '';
+  this.ENERGY_USAGE_LOW_TEMP = 18;
+  this.ENERGY_USAGE_MED_TEMP = 25;
+  this.temperature = this.DEFAULT_TEMPERATURE;
+  this.isPowerSavingModeOn = true;
 }
 
+Thermostat.prototype.getCurrentTemperature = function () {
+  return this.temperature;
+};
+
 Thermostat.prototype.increaseTemperature = function() {
+  if(this._isMaxTemperature()) throw new TypeError(this.ERROR_MSG);
   this.temperature++;
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-  if(this._isMinTemperature()) throw new TypeError('Unable to decrease temperature cause is already set to min');
+  if(this._isMinTemperature()) throw new TypeError(this.ERROR_MSG);
   this.temperature--;
 };
 
+Thermostat.prototype.switchPowerMode = function () {
+  if(this._isTempNeedAdjustment()){this.temperature = this.MAX_SAVING_MODE_TEMP;}
+  this.isPowerSavingModeOn = !this.isPowerSavingModeOn;
+};
+
+Thermostat.prototype.resetTemperature = function () {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.energyUsageStatus = function () {
+  if(this.temperature < this.ENERGY_USAGE_LOW_TEMP){return 'low';}
+  if(this.temperature < this.ENERGY_USAGE_MED_TEMP){return 'medium';}
+  return 'high';
+};
+
 Thermostat.prototype._isMinTemperature = function() {
-  return this.temperature <= MIN_TEMPERATURE;
+  this.ERROR_MSG = 'Unable to decrease temperature cause is already set to min';
+  return this.temperature <= this.MIN_TEMPERATURE;
+};
+
+Thermostat.prototype._isMaxTemperature = function () {
+  if(this.isPowerSavingModeOn){
+    this.ERROR_MSG = 'Unable to increase temperature because power saving mode is on';
+    return this.temperature >= this.MAX_SAVING_MODE_TEMP;
+
+  }
+  else {
+    this.ERROR_MSG = 'Unable to increase temperature because maxmimum temperature reached';
+    return this.temperature >= this.MAX_TEMPERATURE;
+  }
+  return false;
+};
+
+Thermostat.prototype._isTempNeedAdjustment = function () {
+  return this.isPowerSavingModeOn === false && this.temperature > this.MAX_SAVING_MODE_TEMP;
 };
